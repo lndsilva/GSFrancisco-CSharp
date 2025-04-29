75cf6bb036ca23f7813e673c349586888f44574c
lndsilva <lndsilva@hotmail.com>
2025-04-29 13:12:38 -0300

    tela de splash projeto francisco

---
+++ GPSFrancisco/frmSplash.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPSFrancisco
+{
+    public partial class frmSplash : Form
+    {
+        public frmSplash()
+        {
+            InitializeComponent();
+        }
+
+        private void tmrSplash_Tick(object sender, EventArgs e)
+        {
+            pgbPorcentagem.Value = pgbPorcentagem.Value + 10;
+            
+            
+        }
+    }
+}
