d328f91b2f0dc189fc2a8de9ee012a0f75890e88
lndsilva <lndsilva@hotmail.com>
2025-05-16 08:46:19 -0300

    tela de pesquisar

--- GPSFrancisco/Program.cs
+++ GPSFrancisco/Program.cs
@@ -16,7 +16,7 @@ namespace GPSFrancisco
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmGerenciarUsuarios());
+            Application.Run(new frmPesquisarUsuario());
         }
     }
 }
---
+++ GPSFrancisco/frmPesquisarUsuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPSFrancisco
+{
+    public partial class frmPesquisarUsuario : Form
+    {
+        public frmPesquisarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        public void limparCampos()
+        {
+            txtDescricao.Clear();
+            ltbPesquisar.Items.Clear();
+            rdbCodigo.Checked = false;
+            rdbNome.Checked = false;
+            txtDescricao.Focus();
+        }
+    }
+}
